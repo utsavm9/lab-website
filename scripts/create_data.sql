fe157661dded180d7fca3c830d55cdf1e69c1603
Parham Hajzavar <parhamzavar@gmail.com>
2020-02-13 22:39:39 -0800

    create file that initializes db with test values

--- scripts/create_data.sql
+++ scripts/create_data.sql
@@ -3,4 +3,14 @@
 -- Prerequisites
 --   MySQL server is running
 
+insert into users (worker_id, lastname, firstname, password)
+    values ('1234', 'Smith', 'John', 'password11');
 
+insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
+    values ('2', 'favorite color', 'blue','red','orange', 'yellow');
+
+insert into questions (question_id, question_type) 
+    values ('2','a');
+
+insert into text (question_id, question, hint)
+    values ('2', 'favorite color', 'color of sky');

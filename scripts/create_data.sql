2c37445146df90563d57b22a33abc6c8ff39b463
Parham Hajzavar <parhamzavar@gmail.com>
2020-02-14 13:47:48 -0800

    updated test data

--- scripts/create_data.sql
+++ scripts/create_data.sql
@@ -1,9 +1,9 @@
 -- Initializes the database with test values
 
 insert into users (worker_id, lastname, firstname, password_hash)
-    values ('1234', 'Smith', 'John', '1');
+    values ('1234', 'Smith', 'John', 'hash(1)');
 insert into users (worker_id, lastname, firstname, password_hash)
-    values ('5678', 'George', 'Peter', '2');
+    values ('5678', 'George', 'Peter', 'hash(2)');
 
 insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
     values ('1', 'What is the best UC?', 'UCLA','Berkeley','UCSB', 'UCSD');
@@ -12,21 +12,26 @@ insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
 
 insert into questions (question_id, question_type) 
     values ('1', 'm');
-insert into questions (question_id, question_type) 
+insert into questions (question_id, question_type)
     values ('2', 'm');
 insert into questions (question_id, question_type)
-    values ('3', 'm');
+    values ('3', 't');
 insert into questions (question_id, question_type)
-    values ('4', 'i');
+    values ('4', 't');
 insert into questions (question_id, question_type)
-    values ('5', 't');
+    values ('5', 'i');
 insert into questions (question_id, question_type)
-    values ('6', 't');
+    values ('6', 'i');
 
 insert into text (question_id, question, hint)
-    values ('1', "What is the best UC?", 'it is in Westood');
+    values ('3', "What is your favorite weather", 'sun is up');
 insert into text (question_id, question, hint)
-    values ('2', 'What is your favorite color?', 'color of sky');
+    values ('4', 'What is your favorite color?', 'color of sky');
+
+insert into image (question_id, question, image_type, show_for, max_time)
+    values ('5', "What are your thoughts on this image?", "circle", '5', '20');
+insert into image (question_id, question, image_type, show_for, max_time)
+    values ('6', "What does this image remind you of?", "rectangle", '5', '20');
 
 insert into users_experiments (worker_id, experiment_id, trial_id)
     values('1234', '10', '11')
@@ -36,9 +41,9 @@ insert into users_experiments (worker_id, experiment_id, trial_id)
     values('5678', '20', '13')
 
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('10','123456','5');
+    values('10','123456','1');
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('15','134265','5');
+    values('15','134265','3');
 
 insert into responses (trial_id, question_id, start_time, end_time, response)
-    values ('11', '2', '5','')
+    values ('11', '2', '5','blue')

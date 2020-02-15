e6b8d36bb61a04fc89a484e84c94e96a76848107
Parham Hajzavar <parhamzavar@gmail.com>
2020-02-15 09:00:37 -0800

    updated test data

--- backend/database.js
+++ backend/database.js
@@ -86,29 +86,29 @@ exports.addUser = (firstname, lastname, worker_id, password_hash) => {
  * @param {string}  choice_4
  */
 
-exports.getMcqByQuestionsId(question_id, question, choice_1, choice_2, choice_3, choice_4) => {
-    return new Promise((resolve, reject) => {
-        database.query("select * from mcq where question_id=?", question_id, (error, result) => {
-            if (error) return reject(error);
-            resolve(result);
-        });
-    });
-};
+// exports.getMcqByQuestionsId(question_id, question, choice_1, choice_2, choice_3, choice_4) => {
+//     return new Promise((resolve, reject) => {
+//         database.query("select * from mcq where question_id=?", question_id, (error, result) => {
+//             if (error) return reject(error);
+//             resolve(result);
+//         });
+//     });
+// };
 
 /**
  * finds a text from the text table by question_id
- * @param {integer} question_id
- * @param {string} question
- * @param {string} hint
- */
-exports.getTextbyQuestionId(question_id, question, hint) => {
-    return new Promise((resolve, reject) => {
-        database.query("select * from text where question_id=?", question_id, (error, result) => {
-            if (error) return reject(error);
-            resolve(result);
-        });
-    });
-};
+//  * @param {integer} question_id
+//  * @param {string} question
+//  * @param {string} hint
+//  */
+// exports.getTextbyQuestionId(question_id, question, hint) => {
+//     return new Promise((resolve, reject) => {
+//         database.query("select * from text where question_id=?", question_id, (error, result) => {
+//             if (error) return reject(error);
+//             resolve(result);
+//         });
+//     });
+// };
 
 /**
  * finds an image from the image table by question_id 
--- scripts/create_data.sql
+++ scripts/create_data.sql
@@ -1,16 +1,12 @@
 -- Initializes the database with test values
+use experiment_data;
 
-insert into users (worker_id, lastname, firstname, password_hash)
-    values ('1234', 'Smith', 'John', 'hash(1)');
-insert into users (worker_id, lastname, firstname, password_hash)
-    values ('5678', 'George', 'Peter', 'hash(2)');
+insert into users (worker_id, lastname, firstname, password_hash) 
+    values ('1234', 'Terry', 'John', '$2b$10$BWUMJY9Y2PDXhIfe6q57Nu5RMLVsMuys90CamIldJSSIsq3JEK3CC');
+insert into users (worker_id, lastname, firstname, password_hash) 
+    values ('5678', 'Lampard', 'Frank', '$2b$10$TQTjr.O8xO8xKtrI/q4ydutdZJe/ouLHH.edf6N/mzLRNDJnmORDy');
 
-insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
-    values ('1', 'What is the best UC?', 'UCLA','Berkeley','UCSB', 'UCSD');
-insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
-    values ('2', 'What is your favorite color?', 'blue','red','orange', 'yellow');
-
-insert into questions (question_id, question_type) 
+insert into questions (question_id, question_type)
     values ('1', 'm');
 insert into questions (question_id, question_type)
     values ('2', 'm');
@@ -23,27 +19,42 @@ insert into questions (question_id, question_type)
 insert into questions (question_id, question_type)
     values ('6', 'i');
 
+insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
+    values ('1', 'What is the best UC?', 'UCLA','Berkeley','UCSB', 'UCSD');
+insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
+    values ('2', 'What is your favorite color?', 'blue','red','orange', 'yellow');
+
 insert into text (question_id, question, hint)
     values ('3', "What is your favorite weather", 'sun is up');
 insert into text (question_id, question, hint)
     values ('4', 'What is your favorite color?', 'color of sky');
 
 insert into image (question_id, question, image_type, show_for, max_time)
-    values ('5', "What are your thoughts on this image?", "circle", '5', '20');
+    values ('5', "What are your thoughts on this image?", "circle", '00:00:30', '00:05:00');
 insert into image (question_id, question, image_type, show_for, max_time)
-    values ('6', "What does this image remind you of?", "rectangle", '5', '20');
+    values ('6', "What does this image remind you of?", "rectangle", '00:00:30', '00:05:00');
 
 insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('1234', '10', '11')
+    values('1234',  '5', '1');
 insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('5678', '15', '12')
+    values('1234', '10', '2');
 insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('5678', '20', '13')
+    values('1234', '15', '3');
+insert into users_experiments (worker_id, experiment_id, trial_id)
+    values('5678', '20', '4');
+insert into users_experiments (worker_id, experiment_id, trial_id)
+    values('5678', '25', '5');
 
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('10','123456','1');
+    values('5', '135246','1');
+insert into experiments_questions(experiment_id, question_order, question_id)
+    values('10','123456','2');
 insert into experiments_questions(experiment_id, question_order, question_id)
     values('15','134265','3');
+insert into experiments_questions(experiment_id, question_order, question_id)
+    values('20','654321','4');
+insert into experiments_questions(experiment_id, question_order, question_id)
+    values('25','246135','5');
 
 insert into responses (trial_id, question_id, start_time, end_time, response)
-    values ('11', '2', '5','blue')
+    values ('11', '2', '08:00:00','08:10:00','blue');

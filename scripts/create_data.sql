69ef69284eccc1e63c7e5ff40216e90ed5fc166b
Utsav Munendra <utsavm9@gmail.com>
2020-02-16 17:49:30 -0800

    Fixed change in password cloumn name to password_hash, added more edge cases to test data, removed temp files

--- scripts/create_data.sql
+++ scripts/create_data.sql
@@ -20,9 +20,9 @@ insert into questions (question_id, question_type)
     values ('6', 'i');
 
 insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
-    values ('1', 'What is the best UC?', 'UCLA','Berkeley','UCSB', 'UCSD');
+    values ('1', 'What is the best UC?', 'UCLA', 'Berkeley', 'UCSB', 'UCSD');
 insert into mcq (question_id, question, choice_1, choice_2, choice_3, choice_4)
-    values ('2', 'What is your favorite color?', 'blue','red','orange', 'yellow');
+    values ('2', 'What is your favorite color?', 'blue', 'red', 'orange', 'yellow');
 
 insert into text (question_id, question, hint)
     values ('3', "What is your favorite weather", 'sun is up');
@@ -35,26 +35,22 @@ insert into image (question_id, question, image_type, show_for, max_time)
     values ('6', "What does this image remind you of?", "rectangle", '00:00:30', '00:05:00');
 
 insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('1234',  '5', '1');
+    values('1234', '5', '1');
 insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('1234', '10', '2');
+    values('5678', '5', '-1');
 insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('1234', '15', '3');
-insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('5678', '20', '4');
-insert into users_experiments (worker_id, experiment_id, trial_id)
-    values('5678', '25', '5');
+    values('5678', '10', '5');
 
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('5', '135246','1');
+    values('5', '1', '1');
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('10','123456','2');
+    values('5', '2', '2');
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('15','134265','3');
+    values('5', '2', '3');
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('20','654321','4');
+    values('10', '1', '4');
 insert into experiments_questions(experiment_id, question_order, question_id)
-    values('25','246135','5');
+    values('10', '1', '5');
 
 insert into responses (trial_id, question_id, start_time, end_time, response)
-    values ('11', '2', '08:00:00','08:10:00','blue');
+    values ('5', '2', '08:00:00','08:10:00', 'blue');

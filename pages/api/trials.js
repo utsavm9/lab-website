41960675e1d02976d856fdeea67e970f7bbd434b
frank xing <xing_siyuan@hotmail.com>
2020-03-03 20:19:56 -0800

    Trials Page Completed

--- pages/api/trials.js
+++ pages/api/trials.js
@@ -3,5 +3,18 @@ const database = require("../../backend/database");
 export default async (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-    res.end(JSON.stringify({ name: await database.getUserExperimentByWorker_id(1234)}));
+
+    if (req.query.worker_id != null) {
+        res.end(
+            JSON.stringify({
+                name: await database.getUserExperimentByWorker_id(req.query.worker_id)
+            })
+        );
+    } else {
+        res.end(
+            JSON.stringify({
+                name: ''
+            })
+        );
+    }
 };
--- pages/trials.js
+++ pages/trials.js
@@ -1,42 +1,64 @@
-import fetch from 'isomorphic-unfetch'
+import fetch from "isomorphic-unfetch";
+import { deepAccess } from "../util/utils";
 
 class Trials extends React.Component {
     static async getInitialProps(context) {
+        var request = deepAccess(context, ["req", "user", "worker_id"]);
+        if (request != null) {
+            const res = await fetch("http://localhost:3000/api/trials?worker_id=" + request);
+            const data = await res.json();
 
-      const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-      const data = await res.json();
-    
-      console.log(`Show data fetched. Count: ${data.length}`);
-    
+            const result = data.name.filter(name => name.trial_id == -1);
+
+            return {
+                shows: result,
+                logged: true
+            };
+        } else {
+            return {
+                shows: 42,
+                logged: false
+            };
+        }
     }
 
- 
+    table(tableRow) {
+        const { worker_id, experiment_id, trial_id } = tableRow; //destructuring
+        return (
+            <tr>
+                <td>{worker_id}</td>
+                <td>{experiment_id}</td>
+                <td>{trial_id}</td>
+            </tr>
+        );
+    }
     render() {
-       
+        const loggedIn = this.props.logged;
+
         return (
             <>
-            <div>Next stars: {this.data.stargazers_count}</div>
                 <h1>Trials Page</h1>
                 <p>{this.props.errorMessage}</p>
 
                 <form action="/trials" method="GET">
-                  <table>
-                      <thead>
-                          <tr>
-                            <th>Worker ID</th>
-                            <th>Experiment ID</th>
-                            <th>Trial ID</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          <tr>
-                            <td id = "worker_id" name = "worker_id">A123:</td>
-                            <td id = "experiment_id" name = "experiment_id">123:</td>
-                            <td id = "trial_id" name = "trial_id">0:</td>
-                          </tr>
-                      </tbody>
-                      
-                  </table>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Worker ID</th>
+                                <th>Experiment ID</th>
+                                <th>Trial ID</th>
+                            </tr>
+                        </thead>
+                        {loggedIn ? (
+                            <tbody>{this.props.shows.map(element => this.table(element))}</tbody>
+                        ) : (
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        )}
+                    </table>
                 </form>
             </>
         );
